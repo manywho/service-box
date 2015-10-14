241793b24d0ad14bbdb53aaeffa2ef0753d70f55
Jonjo McKay <jonjo.mckay@manywho.com>
2015-10-14 11:17:45 +0100

    CORE-1095: Added missing folder bindings

--- src/main/java/com/manywho/services/box/types/Folder.java
+++ src/main/java/com/manywho/services/box/types/Folder.java
@@ -1,6 +1,10 @@
 package com.manywho.services.box.types;
 
+import com.manywho.sdk.entities.draw.elements.type.TypeElementBinding;
+import com.manywho.sdk.entities.draw.elements.type.TypeElementBindingCollection;
 import com.manywho.sdk.entities.draw.elements.type.TypeElementProperty;
+import com.manywho.sdk.entities.draw.elements.type.TypeElementPropertyBinding;
+import com.manywho.sdk.entities.draw.elements.type.TypeElementPropertyBindingCollection;
 import com.manywho.sdk.entities.draw.elements.type.TypeElementPropertyCollection;
 import com.manywho.sdk.enums.ContentType;
 import com.manywho.sdk.services.describe.types.AbstractType;
@@ -11,6 +15,19 @@ public class Folder extends AbstractType {
     @Override
     public String getDeveloperName() {
         return NAME;
+    }
+
+    @Override
+    public TypeElementBindingCollection getBindings() {
+        return new TypeElementBindingCollection() {{
+            add(new TypeElementBinding(NAME, "Details about a folder", NAME, new TypeElementPropertyBindingCollection() {{
+                add(new TypeElementPropertyBinding("ID", "ID"));
+                add(new TypeElementPropertyBinding("Name", "Name"));
+                add(new TypeElementPropertyBinding("Description", "Description"));
+                add(new TypeElementPropertyBinding("Created At", "Created At"));
+                add(new TypeElementPropertyBinding("Modified At", "Modified At"));
+            }}));
+        }};
     }
 
     @Override
