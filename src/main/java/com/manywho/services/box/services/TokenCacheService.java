1960542a8aecf7a889fdb7dda52e16fa445a8dd4
jose <jose.collazzi@manywho.com>
2016-08-08 13:59:14 +0100

    CORE-2464 test for load file and load folder

--- src/main/java/com/manywho/services/box/services/TokenCacheService.java
+++ src/main/java/com/manywho/services/box/services/TokenCacheService.java
@@ -3,9 +3,6 @@ package com.manywho.services.box.services;
 import com.box.sdk.IAccessTokenCache;
 import com.box.sdk.InMemoryLRUAccessTokenCache;
 
-/**
- * Created by Jose on 08/08/2016.
- */
 public class TokenCacheService {
     public IAccessTokenCache  getAccessTokenCache() {
         return new InMemoryLRUAccessTokenCache(100);
