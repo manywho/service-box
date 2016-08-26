e6ad18fbffec00b933a9e7f060ac8afe57268a00
jose <jose.collazzi@manywho.com>
2016-08-26 09:21:57 +0100

    fix problem with date in tasks

--- src/main/java/com/manywho/services/box/services/TaskService.java
+++ src/main/java/com/manywho/services/box/services/TaskService.java
@@ -1,12 +1,14 @@
 package com.manywho.services.box.services;
 
 import com.box.sdk.BoxFile;
-//import com.box.sdk.BoxTask;
 import com.box.sdk.BoxTask;
 import com.manywho.services.box.facades.BoxFacade;
 import org.joda.time.DateTime;
 
 import javax.inject.Inject;
+import java.util.Date;
+
+//import com.box.sdk.BoxTask;
 
 public class TaskService {
     private BoxFacade boxFacade;
@@ -20,8 +22,13 @@ public class TaskService {
         // Load the requested file from Box so we can add a new task to it
         BoxFile file = boxFacade.getFile(token, fileId);
         if (file != null) {
+            Date date = null;
+            if(dueAt != null) {
+                date = dueAt.toDate();
+            }
+
             // Add a new task to the file with the given details
-            return file.addTask(BoxTask.Action.REVIEW, message, dueAt.toDate());
+            return file.addTask(BoxTask.Action.REVIEW, message, date);
         }
 
         throw new Exception("The requested file could not be loaded from Box");
