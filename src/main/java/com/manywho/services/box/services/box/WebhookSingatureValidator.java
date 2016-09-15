7d2083c7bc76540cafb50fc956a8452e405900f3
jose <jose.collazzi@manywho.com>
2016-09-15 15:04:16 +0100

    add logs about signature keys

--- src/main/java/com/manywho/services/box/services/box/WebhookSingatureValidator.java
+++ src/main/java/com/manywho/services/box/services/box/WebhookSingatureValidator.java
@@ -2,19 +2,28 @@ package com.manywho.services.box.services.box;
 
 import com.box.sdk.BoxWebHookSignatureVerifier;
 import com.manywho.services.box.configuration.SecurityConfiguration;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.message.ParameterizedMessageFactory;
 
 import javax.inject.Inject;
 
 public class WebhookSingatureValidator {
     private SecurityConfiguration securityConfiguration;
     private BoxWebHookSignatureVerifier verifier;
+    private static final Logger LOGGER = LogManager.getLogger(new ParameterizedMessageFactory());
 
     @Inject
     public WebhookSingatureValidator(SecurityConfiguration securityConfiguration) {
         this.securityConfiguration = securityConfiguration;
+        LOGGER.debug("signature keys");
+        LOGGER.debug(StringUtils.length(this.securityConfiguration.getWebhookSignaturePrimaryKey()));
+        LOGGER.debug(StringUtils.length(this.securityConfiguration.getWebhookSignatureSecondaryKey()));
+
         this.verifier =  new BoxWebHookSignatureVerifier(
-                securityConfiguration.getWebhookSignaturePrimaryKey(),
-                securityConfiguration.getWebhookSignatureSecondaryKey()
+                this.securityConfiguration.getWebhookSignaturePrimaryKey(),
+                this.securityConfiguration.getWebhookSignatureSecondaryKey()
         );
     }
 
