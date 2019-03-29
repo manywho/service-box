99f43b96c4c545f217f3887cbd89568d6bc4898f
Jose Collazzi <josecollazzi@gmail.com>
2019-03-29 16:20:51 +0000

    remove encryption

--- src/main/java/com/manywho/services/box/services/EncryptService.java
+++ src/main/java/com/manywho/services/box/services/EncryptService.java
@@ -1,11 +1,7 @@
 package com.manywho.services.box.services;
 
 import com.manywho.services.box.configuration.EncryptConfiguration;
-import org.jose4j.jwa.AlgorithmConstraints;
-import org.jose4j.jwe.ContentEncryptionAlgorithmIdentifiers;
 import org.jose4j.jwe.JsonWebEncryption;
-import org.jose4j.jwe.KeyManagementAlgorithmIdentifiers;
-import org.jose4j.jwk.PublicJsonWebKey;
 import org.jose4j.lang.JoseException;
 
 import javax.inject.Inject;
@@ -16,22 +12,22 @@ public class EncryptService {
 
     @Inject
     public EncryptService(EncryptConfiguration encryptConfiguration) {
-        senderJwe = new JsonWebEncryption();
-        receiverJwe = new JsonWebEncryption();
-
-        try {
-            PublicJsonWebKey key = PublicJsonWebKey.Factory.newPublicJwk(encryptConfiguration.getVerificationKey());
-            senderJwe.setKey(key.getKey());
-            receiverJwe.setKey(key.getPrivateKey());
-
-        } catch (JoseException e) {
-            throw new RuntimeException("Error generating verification.key", e);
-        }
-
-        senderJwe.setAlgorithmHeaderValue(KeyManagementAlgorithmIdentifiers.ECDH_ES_A192KW);
-        senderJwe.setEncryptionMethodHeaderParameter(ContentEncryptionAlgorithmIdentifiers.AES_192_CBC_HMAC_SHA_384);
-        receiverJwe.setAlgorithmConstraints(new AlgorithmConstraints(AlgorithmConstraints.ConstraintType.WHITELIST, KeyManagementAlgorithmIdentifiers.ECDH_ES_A192KW));
-        receiverJwe.setContentEncryptionAlgorithmConstraints(new AlgorithmConstraints(AlgorithmConstraints.ConstraintType.WHITELIST, ContentEncryptionAlgorithmIdentifiers.AES_192_CBC_HMAC_SHA_384));
+//        senderJwe = new JsonWebEncryption();
+//        receiverJwe = new JsonWebEncryption();
+//
+//        try {
+//            PublicJsonWebKey key = PublicJsonWebKey.Factory.newPublicJwk(encryptConfiguration.getVerificationKey());
+//            senderJwe.setKey(key.getKey());
+//            receiverJwe.setKey(key.getPrivateKey());
+//
+//        } catch (JoseException e) {
+//            throw new RuntimeException("Error generating verification.key", e);
+//        }
+//
+//        senderJwe.setAlgorithmHeaderValue(KeyManagementAlgorithmIdentifiers.ECDH_ES_A192KW);
+//        senderJwe.setEncryptionMethodHeaderParameter(ContentEncryptionAlgorithmIdentifiers.AES_192_CBC_HMAC_SHA_384);
+//        receiverJwe.setAlgorithmConstraints(new AlgorithmConstraints(AlgorithmConstraints.ConstraintType.WHITELIST, KeyManagementAlgorithmIdentifiers.ECDH_ES_A192KW));
+//        receiverJwe.setContentEncryptionAlgorithmConstraints(new AlgorithmConstraints(AlgorithmConstraints.ConstraintType.WHITELIST, ContentEncryptionAlgorithmIdentifiers.AES_192_CBC_HMAC_SHA_384));
     }
 
     public String encryptData(String input) throws JoseException {
