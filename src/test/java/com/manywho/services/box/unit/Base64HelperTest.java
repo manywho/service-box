5fd3ea23650589e84cfd117c352e99a4f18bc79c
Jose Collazzi <josecollazzi@gmail.com>
2019-06-03 15:36:14 +0100

    issue #36 using base64 configuration for problematic values

---
+++ src/test/java/com/manywho/services/box/unit/Base64HelperTest.java
@@ -0,0 +1,19 @@
+
+package com.manywho.services.box.unit;
+
+import com.manywho.services.box.utilities.Base64Helper;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Base64HelperTest {
+
+    @Test
+    public void testBase64EncodeDecode() {
+
+        String text = "aa@--@@%6&*%$£\n,,.";
+        String encoded = Base64Helper.encode(text);
+
+        Assert.assertEquals(text, Base64Helper.decode(encoded));
+
+    }
+}
