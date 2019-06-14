8b48b4496c055f06478e70baa0064f1f09a85280
Jose Collazzi <josecollazzi@gmail.com>
2019-06-14 10:31:40 +0100

    issue #40 removing buncycastle from pom and bc provider from decrypted key

--- src/main/java/com/manywho/services/box/services/EncryptService.java
+++ src/main/java/com/manywho/services/box/services/EncryptService.java
@@ -1,15 +1,14 @@
 package com.manywho.services.box.services;
 
 import com.manywho.services.box.configuration.EncryptConfiguration;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.jose4j.jwa.AlgorithmConstraints;
 import org.jose4j.jwe.ContentEncryptionAlgorithmIdentifiers;
 import org.jose4j.jwe.JsonWebEncryption;
 import org.jose4j.jwe.KeyManagementAlgorithmIdentifiers;
 import org.jose4j.jwk.PublicJsonWebKey;
 import org.jose4j.lang.JoseException;
+
 import javax.inject.Inject;
-import java.security.Security;
 
 public class EncryptService {
     private JsonWebEncryption senderJwe;
@@ -17,7 +16,6 @@ public class EncryptService {
 
     @Inject
     public EncryptService(EncryptConfiguration encryptConfiguration) {
-        Security.addProvider(new BouncyCastleProvider());
 
         senderJwe = new JsonWebEncryption();
         receiverJwe = new JsonWebEncryption();
